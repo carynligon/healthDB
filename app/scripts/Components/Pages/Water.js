40b930d8bd8b9e6eb2edac11ee91e4b6c4ce4912
Caryn Ligon <carynligon@gmail.com>
2016-10-04 12:25:24 -0500

    switched to line graph

--- app/scripts/Components/Pages/Water.js
+++ app/scripts/Components/Pages/Water.js
@@ -3,10 +3,22 @@ import {Link} from 'react-router';
 
 import Sidebar from '../Sidebar';
 import WaterBar from '../WaterBar';
-import WaterPie from '../WaterPie';
+import WaterLine from '../WaterLine';
 
 export default React.createClass({
   data: [56, 75, 80, 46, 26, 75, 21, 59],
+  lineData: [
+    {day: 1, voume: 43},
+    {day: 2, voume: 23},
+    {day: 3, voume: 66},
+    {day: 4, voume: 54},
+    {day: 5, voume: 78},
+    {day: 6, voume: 80},
+    {day: 7, voume: 32},
+    {day: 8, voume: 47},
+    {day: 9, voume: 54},
+    {day: 10, voume: 78},
+  ],
   render() {
     return (
       <main>
@@ -17,7 +29,7 @@ export default React.createClass({
             <h2>Water</h2>
           </header>
           <WaterBar data={this.data}/>
-          <WaterPie data={this.data}/>
+          <WaterLine data={this.lineData}/>
         </div>
       </main>
     )
--- app/scripts/Components/WaterLine.js
+++ app/scripts/Components/WaterLine.js
@@ -5,7 +5,6 @@ var d3 = require('d3');
 export default React.createClass({
   mixins: [Faux.mixins.core, Faux.mixins.anim],
   getInitialState: function () {
-    console.log(d3);
     return { look: 'stacked' }
   },
   render: function () {
@@ -19,17 +18,27 @@ export default React.createClass({
     let margin = {top: 40, right: 10, bottom: 20, left: 10};
     let width = 960 - margin.left - margin.right;
     let height = 350 - margin.top - margin.bottom;
-
-    let x = d3.scaleOrdinal()
-        .domain([0,10])
-        .range([0, width]);
+    console.log(d3);
+    let x = d3.scaleTime()
+    .range([0, width]);
 
     let y = d3.scaleLinear()
-        .domain([0, 80])
-        .range([height, 0]);
+    .range([height, 0]);
 
-    let color = d3.scaleOrdinal()
-      .range(['#34eadc', '#fb054f', '#2ecc23']);
+    let xAxis = d3.axisBottom()
+        .scale(x)
+        .tickSize(1)
+        .ticks(7)
+        .tickPadding(6);
+
+    let yAxis = d3.axisLeft()
+        .scale(y)
+        .tickSize(1)
+        .tickPadding(6);
+
+    let heightScale = d3.scaleLinear()
+        .domain([0,80])
+        .range([0, height]);
 
     let svg = d3.select(faux).append('svg')
         .attr('class', 'water-bar')
@@ -38,29 +47,24 @@ export default React.createClass({
       .append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-    let group = svg.append('g')
-      .attr('transform', 'translate(300,200)');
+    console.log(this.props);
+    let line = d3.line()
+        .x(function(d) { return x(d.day); })
+        .y(function(d) { return y(d.volume); });
 
-    let arc = d3.arc()
-      .innerRadius(200)
-      .outerRadius(r);
+    svg.append("path")
+      .datum(this.props.lineData)
+      .attr("class", "line")
+      .attr("d", line);
 
-    let pie = d3.pie()
-      .value(function(d) { return d; });
 
-    let arcs = group.selectAll('.arc')
-      .data(pie(this.props.data))
-      .enter()
-      .append('g')
-      .attr('class', 'arc')
+    svg.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis);
 
-    arcs.append('path')
-      .attr('d', arc)
-      .attr('fill', function(d) {
-        return color(d)
-      });
-
-    let r = 300;
-    let p = Math.PI * 2;
+    svg.append('g')
+        .attr('class', 'y axis')
+        .call(yAxis);
   }
 });
